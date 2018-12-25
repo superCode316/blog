eeb18ccaae56f873b9068a516af75b67a30555d8
钱曹宇 <name20000316@gmail.com>
2018-12-25 21:41:26 +0800

    增加验证码

--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,22 @@
 let express = require('express');
 const { check, validationResult } = require('express-validator/check');
-
+let svgCaptcha = require('svg-captcha');
 let router = express.Router();
-
+let captcha = svgCaptcha.create();
+let verify = function(){
+    captcha = svgCaptcha.create();
+};
 let Article = require('../models/article');
 router.get('/new',function(req,res){
+    verify();
     res.render('articles/new',{
-        title:'add article'
+        data:{
+            title:'',
+            author:'',
+            body:''
+        },
+        title:'add article',
+        svg_data:captcha.data
     });
 });
 
@@ -16,21 +26,37 @@ router.post('/create',[check('title').isLength({min:1}).withMessage("标题字�
     function(req,res){
         let article = new Article(req.body);
         const errors = validationResult(req);
+        let error = errors.array();
+        if(req.body.verify != captcha.text){
+            error.push({
+                location:'body',
+                param:'verify',
+                value:req.body.verify,
+                msg:'验证码输入错误'
+            });
+        }
+        // console.log(error);
         // console.log(req.body);
+        // console.log(captcha.text);
         // console.log(errors.array());
-        article.save(function(err){
-            if(errors.isEmpty()){
-                {
-                    res.redirect('/');
-                    req.flash('success','Article Added');
+        if(error.length === 0){
+            article.save(function(err){
+                if (err) {
+                    console.log(err);
                 }
-            }else {
-                res.render('articles/new',{
-                    title:'Add Article',
-                    errors:errors.array()
-                });
-            }
-        })
+                res.redirect('/');
+                req.flash('success','Article Added');
+
+            })
+        }else {
+            verify();
+            res.render('articles/new',{
+                data:req.body,
+                title:'Add Article',
+                errors:error,
+                svg_data:captcha.data
+            });
+        }
     });
 
 router.get('/:id/edit',function(req,res){
@@ -38,8 +64,9 @@ router.get('/:id/edit',function(req,res){
         if(err){
             return
         }
+        req.flash('message','Article Added')
         res.render('articles/edit',{
-            article:article
+            article:article,
         })
     })
 });
